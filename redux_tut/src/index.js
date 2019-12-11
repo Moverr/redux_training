dadd3a2ea1bf43badb11de41000f7e29d2685701
Muyinda Rogers <moverr@gmail.com>
2019-12-11 16:50:54 +0300

    :Debugging :

--- redux_tut/src/index.js
+++ redux_tut/src/index.js
@@ -4,28 +4,45 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
- import {createStore} from 'redux';
+import { createStore } from 'redux';
 
 //STORE : GLOBALIZED STATE
 
 
 //ACTION : Describes what you intend to do : a simple function that returns an object 
 
-const increment = ()=>{
-    return{
-        type:'INCREMENT'
+const increment = () => {
+    return {
+        type: 'INCREMENT'
     }
 }
 
 
-const decrement = ()=>{
-    return{
-        type:'DECREMENT'
+const decrement = () => {
+    return {
+        type: 'DECREMENT'
     }
 }
 
 //REDUCER : Describes how you action transform the state into the next state 
 
+const counter = (state = 0, action) => {
+
+    switch (action.type) {
+        case "DECREMENT":
+            return state - 1;
+            break;
+
+        case "INCREMENT":
+            return state + 1;
+            break;
+
+
+        default:
+            return state;
+            break;
+    }
+}
 //DISPATCHER : 
 
 

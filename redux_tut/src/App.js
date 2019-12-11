913b29cc779d961f1f5a2b444b581f89d6f31fcc
Muyinda Rogers <moverr@gmail.com>
2019-12-11 20:55:21 +0300

    :Debugging,  Increment

--- redux_tut/src/App.js
+++ redux_tut/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react'; 
- 
+
+ import { useSelector,useDispatch } from 'react-redux'
+
+
+ import {increment,decrement} from './actions'
 
 
 
 function App() {
+  const counter = useSelector(state =>state.counterReducer)
+  const dispatch = useDispatch();
   return (
     <div className="App">
-       <h1> Redux Tutorial </h1>
+       <h1> Redux Tutorial {counter} </h1>
+       <button onClick={()=> dispatch(increment()) }>+</button>
+       <button onClick={()=> dispatch(decrement()) } >-</button>
+       
     </div>
   );
 }
